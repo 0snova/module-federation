c87ab1681fb0948ec0c13dd4f91778755c77a6c0
Alexander Snov <snov.contact@gmail.com>
2021-09-25 13:19:24 +0700

    Add support custom fallback, ready and failed state components

--- src/RemoteComponent.tsx
+++ src/RemoteComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, SuspenseProps, ReactNode } from 'react';
 import { loadScript } from './loadScript';
 import { initRemoteModule } from './initRemoteModule';
 
@@ -33,29 +33,40 @@ export type RemoteComponentProps<T> = T & {
   url?: string;
   urlDev?: string;
   forceUrl?: string;
+  fallback?: SuspenseProps['fallback'];
+  notReadyComponent?: React.ReactNode | null;
+  failedComponent?: React.ReactNode | null;
 };
 
-export function RemoteComponent<T>({ url, urlDev, forceUrl, scope, module, ...props }: RemoteComponentProps<T>) {
+export function RemoteComponent<T>({
+  fallback = null,
+  notReadyComponent = null,
+  failedComponent = null,
+  url,
+  urlDev,
+  forceUrl,
+  scope,
+  module,
+  ...props
+}: RemoteComponentProps<T>) {
   const selectedUrl = forceUrl ?? ((process.env as any).isProduction ? url : urlDev);
   if (!selectedUrl) {
-    throw new Error(`Unable to get RemoteComponent url ${selectedUrl}`);
+    throw new Error(`Unable to get URL for RemoteComponent module "${module}": ${selectedUrl}`);
   }
   const { failed, ready } = useDynamicScript(selectedUrl);
 
   if (!ready) {
-    console.log('RemoteComponent is not readt yet');
-    return <div>loading script {selectedUrl}</div>;
+    return <>{notReadyComponent}</>;
   }
 
   if (failed) {
-    console.log('RemoteComponentfailed');
-    return <div>failed to load script {selectedUrl}</div>;
+    return <>{failedComponent}</>;
   }
 
   const Component = React.lazy(initRemoteModule(scope, module));
 
   return (
-    <React.Suspense fallback="Loading System">
+    <React.Suspense fallback={fallback}>
       <Component {...props} />
     </React.Suspense>
   );
